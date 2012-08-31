ae489a66350493f9961df688874eca05e6d54d93
Nil <305481765@qq.com>
2012-08-31 15:11:03 +0800

    add a test demo...
    Signed-off-by: Nil <305481765@qq.com>

---
+++ TestJavaApp/src/nil/test/main/TetsApp.java
@@ -0,0 +1,13 @@
+package nil.test.main;
+
+public class TetsApp {
+	public static void main(String[] args) {
+
+	}
+	
+	public void TestStart(){
+		@SuppressWarnings("unused")
+		int test = 12;
+		System.out.println("------------");
+	}
+}
